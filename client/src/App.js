eb11f9883b83e8cc765f10c927794c5d124fd794
Pawanwat <107258927+Pawanwat@users.noreply.github.com>
2024-08-02 13:39:51 +0700

    Add Router-dom. Create Homepage and Product Screen page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './pages/HomeScreen';
+import ProductScreen from "./pages/ProductScreen";
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
         <a href="/">amazona</a>
       </header>
       <main>
-        list products
+        <Routes>
+          <Route path="/" element={<HomeScreen />}/>
+          <Route path="/product/:slug" element={<ProductScreen/>} />
+        </Routes>
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            coutnInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 150,
+            coutnInStock: 1,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 14,
+            description: 'high quality pant',
+        },
+        {
+            name: 'Adidas Slim Shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 130,
+            coutnInStock: 5,
+            brand: 'Adidas',
+            rating: 4.6,
+            numReviews: 13,
+            description: 'high quality shirt',
+        }
+    ]
+}
+
+export default data;
