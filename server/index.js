7f778a0e52e126732797a154199552579aaa5a03
Pawanwat <107258927+Pawanwat@users.noreply.github.com>
2024-08-12 15:16:42 +0700

    Add Procfile for preaparing deploy to heroku. and Add configuration for prodcution

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -29,6 +30,12 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+})
 
 const port = process.env.PORT || 5000;
 
